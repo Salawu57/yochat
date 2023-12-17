d5ff29341be328a0ad49e7e91d41afdfea01c363
Salawu Babatunde Afees <fisco57@yahoo.com>
2023-12-17 14:59:29 +0100

    completed ui

--- lib/authentication/screens/profile/profile.dart
+++ lib/authentication/screens/profile/profile.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:yo_chat/authentication/screens/profile/widgets/profile_menu.dart';
+import 'package:yo_chat/common/widgets/Headings/SectionHeading.dart';
 import 'package:yo_chat/common/widgets/appbar/appbar.dart';
 import 'package:yo_chat/common/widgets/custom_shapes/circular_image/circular_image.dart';
 import 'package:yo_chat/utils/constants/images_strings.dart';
@@ -9,17 +11,84 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: YoAppBar(showBackarrow: true, title: Text('Profile')),
-      body:SingleChildScrollView(
+    return Scaffold(
+      appBar: const YoAppBar(showBackarrow: true, title: Text('Profile')),
+      body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.all(YoAppSize.defaultSpace),
+          padding: const EdgeInsets.all(YoAppSize.defaultSpace),
           child: Column(
             children: [
-              CircularImage(image: YoAppImages.user)
+              SizedBox(
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    const CircularImage(
+                        image: YoAppImages.user, width: 80, height: 80),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text('Change Profile Picture'),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(height: YoAppSize.spaceBtwItems / 2),
+              const Divider(),
+              const SizedBox(height: YoAppSize.spaceBtwItems),
+              const SectionHeading(
+                title: "Profile Information",
+                showActionButton: false,
+              ),
+              const SizedBox(height: YoAppSize.spaceBtwItems),
+              ProfileMenu(
+                title: 'Name',
+                value: 'Babatunde Afees',
+                onPressed: () {},
+              ),
+              ProfileMenu(
+                title: 'Username',
+                value: 'fisco57',
+                onPressed: () {},
+              ),
+              const SizedBox(height: YoAppSize.spaceBtwItems / 2),
+              const Divider(),
+              const SizedBox(height: YoAppSize.spaceBtwItems),
+              const SectionHeading(
+                title: "Personal Information",
+                showActionButton: false,
+              ),
+              const SizedBox(height: YoAppSize.spaceBtwItems),
+              ProfileMenu(
+                title: 'UserID',
+                value: '13434',
+                onPressed: () {},
+              ),
+              ProfileMenu(
+                title: 'E-mail',
+                value: 'fisco57@gmail.com',
+                onPressed: () {},
+              ),
+              ProfileMenu(
+                title: 'Phone Number',
+                value: '+23480384934923',
+                onPressed: () {},
+              ),
+              ProfileMenu(
+                title: 'Gender',
+                value: 'Male',
+                onPressed: () {},
+              ),
+              const Divider(),
+              const SizedBox(height: YoAppSize.spaceBtwItems),
+              Center(
+                child: TextButton(
+                    onPressed: () {},
+                    child: const Text('Close Account',
+                    style: TextStyle(color: Colors.red, fontSize: 16, fontWeight: FontWeight.w500))),
+              )
             ],
-          ),),
-      )
+          ),
+        ),
+      ),
     );
   }
 }
